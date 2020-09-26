ea761bdf2db03e455b5ae2c8ccb6a452ad49a03d
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-26 20:34:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const app = "I don't do much."
 document.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
+
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
